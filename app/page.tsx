98a72405dbb776025be2273b235f1bc3e34be1c0
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-04-04 13:01:36 +0300

    comparision to axios vs fetch for data fetching

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,7 @@
 import React, { useEffect, useState } from "react"
 import Card from "./components/Card"
 
-interface Movie {
-  orginal_title: string
-  release_data: Date
-  vote_average: any
-  overview: string
-}
-
-const Page: React.FC = () => {
+const Page = () => {
   const [movies, setMovies] = useState([])
 
   useEffect(() => {
@@ -19,11 +12,25 @@ const Page: React.FC = () => {
         const res = await fetch(
           `https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`
         )
+        // ? can do the same with axios as follows
+        // try {
+        //   const response = await axios.get(
+        //     "https://api.themoviedb.org/3/movie/popular",
+        //     {
+        //       params: {
+        //         api_key: "4e44d9029b1270a757cddc766a1bcb63",
+        //         language: "en-US",
+        //       },
+        //     }
+        //   );
+        // const data = response.data.results;
+        // setMovies(data);
         if (!res.ok) {
           throw new Error("HTTP error! status: " + res.status)
         }
         const data = await res.json() // Await the promise to get the actual data
         setMovies(data.results)
+        console.log(data)
       } catch (error) {
         console.error("Error fetching data", error)
       }
@@ -32,11 +39,11 @@ const Page: React.FC = () => {
   }, [])
 
   return (
-    <>
+    <div className="grid grid-cols-4 gap-4">
       {movies.map((movie, index) => (
         <Card key={index} movie={movie} />
       ))}
-    </>
+    </div>
   )
 }
 
